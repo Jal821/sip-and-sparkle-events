b7f94b5e9dc31aed86ec6abacdd40551e9343c71
Jal821 <69259126+Jal821@users.noreply.github.com>
2026-03-15 15:45:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,8 @@ import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import NotFound from "./pages/NotFound";
 
-// IMPORTANT: basename must match your GitHub Pages path.
-// If site is at https://jal821.github.io/cocktails4you/ use "/cocktails4you".
-// If you later move to https://jal821.github.io/ only, change to "/".
-const basename = "/cocktails4you";
-
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/blog" element={<Blog />} />
