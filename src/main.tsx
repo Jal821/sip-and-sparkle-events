3631a0adf8032c09d54de5bc013ce648390f2f0c
Jal821 <69259126+Jal821@users.noreply.github.com>
2026-03-15 13:11:47 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,24 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import Index from "./pages/Index";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
+import NotFound from "./pages/NotFound";
+
+// IMPORTANT: basename must match your GitHub Pages path.
+// If site is at https://jal821.github.io/cocktails4you/ use "/cocktails4you".
+// If you later move to https://jal821.github.io/ only, change to "/".
+const basename = "/cocktails4you";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={basename}>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/:slug" element={<BlogPost />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
